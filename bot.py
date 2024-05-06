2ab2fa6ff51bad6785ff23e5c42daa922dfd4e4c
makhmudjonfoziljonov <mahmudjonfoziljonv1@gmail.com>
2024-05-06 14:49:47 +0500

    <some commit message>

--- bot.py
+++ bot.py
@@ -1,17 +1,56 @@
-from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, CallbackContext
-from telegram import Update
+import logging
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+import requests
 from integrations import Constants
 
+city = None
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('🌆Name the city you want to know')
+TELEGRAM_BOT_TOKEN = "TOKEN"
+WEATHER_API_KEY = 'KEY'
 
 
-def main():
-    updater = Updater(Constants.TELEGRAM_BOT_TOKEN, use_context=True)
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("🌆In which city do you live?")
+
+
+def menu(update: Update, context: CallbackContext) -> None:
+    global city
+    city = update.message.text
+    keyboard = [
+        [InlineKeyboardButton("Current Weather", callback_data='current_weather')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('👇Choose an option:', reply_markup=reply_markup)
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+
+    if query.data == 'current_weather':
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}")
+
+        if response.status_code == 200:
+            data = response.json()
+            main = data['main']
+            weather_data = data['weather'][0]
+            celsius_temp = main['temp'] - 273.15
+            message = f"<b>🌤Current weather in {city}</b>:\n\n"
+            message += f"<b>🌡Temperature: {celsius_temp:.2f}°C</b>\n\n"
+            message += f"<b>Description: {weather_data['description'].capitalize()}</b>\n\n"
+            message += f"<b>Humidity: {main['humidity']}%</b>\n"
+            query.edit_message_text(text=message, parse_mode='HTML')
+        else:
+            query.edit_message_text(text="Can't find weather information for this city. Try again.")
+
+
+def main() -> None:
+    updater = Updater(token=Constants.TELEGRAM_BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler(Constants.COMMAND_START, start))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, menu))
+    dp.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
     updater.idle()
--- integrations.py
+++ integrations.py
@@ -2,24 +2,9 @@ import requests
 
 
 class Constants:
-    TELEGRAM_BOT_TOKEN = "5677756493:AAFxIsGxpmF4h8nabX_cdX6xx4hNDkwLpz0"
-    WEATHER_API_KEY = '9154bdf34f1724dade7661f0ae461190'
+    TELEGRAM_BOT_TOKEN = "TOKEN"
+    WEATHER_API_KEY = 'KEY'
     ABOUT_MESSAGE = 'Created by chivalrous'
     COMMAND_START = 'start'
 
 
-def get_weather_data(city):
-    response = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={Constants.WEATHER_API_KEY}")
-    if response.status_code == 200:
-        data = response.json()
-        city = data['name']
-        main = data['weather'][0]['main']
-        temp = round(data['main']['temp'] - 273, 2)
-        speed = data['wind']['speed']
-        text = (f"<b>🏙City name</b>: {city}\n\n"
-                f"<b>🌤Information about weather</b>: {main}\n\n"
-                f"<b>🌡️Current temperature</b>: {temp}°C\n\n"
-                f"<b>💨Wind speed</b>: {speed} km/h")
-        return text
-    return "Please enter the correct city name"
